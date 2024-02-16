38f3f66f3ada2d775848463a15e4d71f2d97cda1
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-02-16 23:34:08 +0530

    First commit

--- Bit_manipulation/hackerearth/2.cpp
+++ Bit_manipulation/hackerearth/2.cpp
@@ -1,1 +1,15 @@
 // Count the number of ones in the binary representation of the given number.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    while(n){
+
+        n = n & (n-1);
+    }
+}
