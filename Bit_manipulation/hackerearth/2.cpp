4097efaff15edddfd31f0180a07dc4e8b40308cd
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-02-16 23:38:32 +0530

    First commit

--- Bit_manipulation/hackerearth/2.cpp
+++ Bit_manipulation/hackerearth/2.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 int Count_one(int n){
-    
+
     int count = 0;
     while(n){
 
         n = n & (n-1);
+        count++;
     }
+    return count;
     
 }
 
 int main(){
 
-    
+    int m = Count_one(23);
+    cout<<m<<endl;
 }
