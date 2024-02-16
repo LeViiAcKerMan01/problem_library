963f8309208c3c794570596a17281b098ff837b6
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-02-16 23:31:24 +0530

    First commit

---
+++ Bit_manipulation/hackerearth/1.cpp
@@ -0,0 +1,25 @@
+// How to check if a given number is a power of 2 ?
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfTwo(int x)
+{
+    if(x == 0)
+        return false;
+    else
+    {
+        while(x % 2 == 0) x /= 2;
+        return (x == 1);
+    }
+}
+
+int main(){
+
+    bool n = isPowerOfTwo(0);
+    bool m = isPowerOfTwo(64);
+
+    cout<< n << " " << m << endl;
+    
+ 
+}
