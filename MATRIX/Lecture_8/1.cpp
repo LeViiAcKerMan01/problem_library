02075352889c19b87c09faf0537c13fa74c08759
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 22:57:35 +0530

    First commit

---
+++ MATRIX/Lecture_8/1.cpp
@@ -0,0 +1,23 @@
+// Search in a row-wise and column-wise sorted matrix
+
+void search(int mat[R][C], int x){
+
+    bool isFound = false;
+
+    for(int i = 0; i < R; i++){
+
+        for(int j = 0; j < C; j++){
+
+            if(mat[i][j] == x){
+
+                cout<<"Found at "<<i<<j<<" ";
+                isFound = true;
+                break;
+            }
+        }
+    }
+    if(isFound == false){
+
+        cout<<"Not Found"<<endl;
+    }
+}
