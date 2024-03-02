ba985a6aaad4c509d638c8c3a6e9099551fdea54
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 16:17:45 +0530

    First commit

---
+++ MATRIX/Lecture_5/1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int n = 3;
+
+void TransposeMatrix(int mat[n][n]){
+
+    int temp[n][n];
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            temp[i][j] = mat[j][i];
+        }
+    }
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            mat[i][j] = temp[i][j];
+        }
+    }
+    // Printing the matrix of order 3
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < n; j++){
+
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+
+    int mat[n][n] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+    TransposeMatrix(mat);
+
+    return 0;
+}
