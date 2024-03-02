13f315119796236a6717d2b003514e3f36a6d3d1
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 14:13:08 +0530

    First commit

---
+++ MATRIX/Lecture_2/1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Print(int mat[3][2]){
+
+    for(int i = 0; i < 3; i++){
+
+        for(int j = 0; j < 2; j++){
+
+            cout<<mat[i][j]<<" ";
+        }
+    }
+}
+
+int main(){
+
+    int mat[3][2] = {{10, 20}, {30, 40}, {50, 60}};
+
+    Print(mat);
+
+    return 0;
+}
+
+// OUTPUT : 10 20 30 40 50 60 
