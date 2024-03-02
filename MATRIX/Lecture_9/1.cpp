6aa2105fd8396e6c7980aaec561f6725e9c9dbd3
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 23:22:48 +0530

    First commit

---
+++ MATRIX/Lecture_9/1.cpp
@@ -0,0 +1,11 @@
+// MEDIAN OF A ROW-WISE SORTED MATRIX
+
+// ASSUMPTIONS :
+
+// odd sized matrix
+// all distinct elements
+
+// NAIVE SOLUTION 
+// Put all the elements in an array O(R * C)
+// Sort the array O(R * log(R * C))
+// Return the middle element of a sorted array O(1)
