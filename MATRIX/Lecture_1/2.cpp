547c4660e04bba2dbede076bd16d48a210548bcf
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 13:19:25 +0530

    First commit

---
+++ MATRIX/Lecture_1/2.cpp
@@ -0,0 +1,29 @@
+// VARIABLE SIZED ARRAYS
+// From C++ 14 standard , C++ allows variable sized arrays also referred as dynamic arrays
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int m = 3;
+    int n = 2;
+    int arr[m][n];
+
+    for(int i = 0; i < m; i++){
+
+        for(int j = 0; j < n; j++){
+
+            arr[i][j] = i+j;
+        }
+    }
+
+    // Print the 2-D array
+    for(int i = 0; i < m; i++){
+
+        for(int j = 0; j < n; j++){
+
+            cout<<arr[i][j]<<" ";
+        }
+    }
+    return 0;
+}
