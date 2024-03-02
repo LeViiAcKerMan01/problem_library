25bd51d1118ace86be1eda2f48325ff6b5d42fc7
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 13:40:11 +0530

    First commit

--- 5.cpp
+++ 5.cpp
@@ -5,5 +5,25 @@ int main(){
 
     int m = 3;
     int n = 2;
-    vector<int> arr[m]; // created a vector having m rows
+    vector<int> arr[m]; 
+    // created a vector having m rows
+    
+
+    for(int i = 0; i < m; i++){
+
+        for(int j = 0; j < n; j++){
+
+            arr[i].push_back(10);
+        }
+    }
+
+    // Print the 2-D array
+
+    for(int i = 0; i < m; i++){
+
+        for(int j = 0; j < n; j++){
+
+            cout<<arr[i][j]<<" ";
+        }
+    }
 }
