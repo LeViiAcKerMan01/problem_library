febfe1adee709e14998d26806a1193d6addf18a7
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-03-02 23:08:44 +0530

    First commit

--- MATRIX/Lecture_8/2.cpp
+++ MATRIX/Lecture_8/2.cpp
@@ -1,7 +1,37 @@
 // EFFICIENT SOLUTION 
 // Time Complexity : O(R + C)
 
-void search(int mat[R][C]){
 
-    
+// Approach explained :
+
+// 1- begin from the top right corner
+// 2- If x is same, print position and return
+// 3- If x is smaller, move left
+// 4- if x is greater, move down
+
+void search(int mat[R][C], int x){
+
+    int i = 0, j = C-1;
+    bool isFound = false;
+
+    while(i < R && j >= 0){
+
+        if(mat[i][j] == x){
+
+            cout<<"found at C"<<i<<" "<<j<<endl;
+            isFound = true;
+            break;
+        }
+        else if(mat[i][j] > x){
+
+            j--;
+        }
+        else{
+            i++;
+        }
+    }
+    if(isFound == false){
+
+        cout<<"Not Found"<<endl;
+    }
 }
